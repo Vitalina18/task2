cae4586ca70b4010344f68c6adf3c9e0d487aedf
malk <a.a.malkin@mai.ru>
2018-03-29 14:44:25 +0300

    Исправили ошибки

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
  
-//const vit=[];
+// const vit=[];
 const mongoose = require('mongoose'); 
 
-mongoose.connect("mongodb//localhost:27017/vitalina"); // Insert your URI here 
+// // mongoose.connect("mongodb//localhost:27017/vitalina"); // Insert your URI here 
+mongoose.connect("mongodb://vitalina:18@ds127129.mlab.com:27129/heroku_jcd5g7s1"); // Insert your URI here 
 const db = mongoose.connection; 
 db.on('error', console.error.bind(console, 'connection error:')); 
 db.once('open', function() { 
@@ -31,35 +32,43 @@ const Vit = mongoose.model('Vit', userSchema);
 app.get('/', function(req, res) {
 	res.render('home');
 });
+
 app.get('/vit', function(req, res) {
 	//res.render('vit', {vit : vit}); 
 	Vit.find(function (err, Vit) { 
-	    if (err) return console.error(err); 
-		 res.render('Vit', {Vit : Vit}); 	
+		if (err) return console.error(err); 
+		
+		console.log(Vit);
+		res.render('Vit', {Vit : Vit}); 	
+	});
 });
 
-app.post("/vvv",urlencodedParser, function (request, response) {
-let name= request.body.name;
-let Lname= request.body.Lname;
-//if (vit.includes(name)) { 
-//	  response.render('error', {add : true, name : name}); 
-//	    } else {    
-//		 vit.push(`${name}  ${Lname}`); 
-//	  response.render('vit', {vit : vit}); 
-//	 } 
-Vit.find({name : name}, function (err, users) { 
-        if (err) return console.error(err); 
-	          if (users.length) { 
-	            res.render('errUser', {add : true, name : name});             
-	         } else { 
-	           newVit = new Vit({name : name}); 
-	            newVit.save(function (err){ 
-	            if (err) return console.error(err); 
-				Vit.find(function (err, Vit) { 
-	             if (err) return console.error(err); 
-               res.render('Vit', {Vit : Vit}); 
-            }) 
-           }) 
-	       } 
-	 });
-app.listen(PORT);
+
+// app.post("/vvv",urlencodedParser, function (request, response) {
+// 	let name= request.body.name;
+// 	let Lname= request.body.Lname;
+// 	//if (vit.includes(name)) { 
+// 	//	  response.render('error', {add : true, name : name}); 
+// 	//	    } else {    
+// 	//		 vit.push(`${name}  ${Lname}`); 
+// 	//	  response.render('vit', {vit : vit}); 
+// 	//	 } 
+// 	Vit.find({name : name}, function (err, users) { 
+// 		if (err) return console.error(err); 
+// 		if (users.length) { 
+// 			res.render('errUser', {add : true, name : name});             
+// 		} else { 
+// 			newVit = new Vit({name : name}); 
+// 			newVit.save(function (err){ 
+// 				if (err) return console.error(err); 
+// 				Vit.find(function (err, Vit) { 
+// 					if (err) return console.error(err); 
+// 					res.render('Vit', {Vit : Vit}); 
+// 				}) 
+// 			}) 
+// 		} 
+// 	});
+// }
+app.listen(PORT, () => {
+	console.log(`app started on port ${PORT}`);
+});
