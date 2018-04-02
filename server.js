8ef4977db9f6d5d114ea29fd53a9066e321e1d9e
Виталина <Vitalinku@mail.ru>
2018-04-02 10:52:26 +0300

    добавление в БД

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + "/public"));
 const mongoose = require('mongoose'); 
 
 // // mongoose.connect("mongodb//localhost:27017/vitalina"); // Insert your URI here 
-mongoose.connect("mongodb://vitalina:18@ds127129.mlab.com:27129/heroku_jcd5g7s1"); // Insert your URI here 
+mongoose.connect("mongodb://vitalina:18@ds127589.mlab.com:27589/vitalina"); // Insert your URI here 
 const db = mongoose.connection; 
 db.on('error', console.error.bind(console, 'connection error:')); 
 db.once('open', function() { 
@@ -44,31 +44,25 @@ app.get('/vit', function(req, res) {
 });
 
 
-// app.post("/vvv",urlencodedParser, function (request, response) {
-// 	let name= request.body.name;
-// 	let Lname= request.body.Lname;
-// 	//if (vit.includes(name)) { 
-// 	//	  response.render('error', {add : true, name : name}); 
-// 	//	    } else {    
-// 	//		 vit.push(`${name}  ${Lname}`); 
-// 	//	  response.render('vit', {vit : vit}); 
-// 	//	 } 
-// 	Vit.find({name : name}, function (err, users) { 
-// 		if (err) return console.error(err); 
-// 		if (users.length) { 
-// 			res.render('errUser', {add : true, name : name});             
-// 		} else { 
-// 			newVit = new Vit({name : name}); 
-// 			newVit.save(function (err){ 
-// 				if (err) return console.error(err); 
-// 				Vit.find(function (err, Vit) { 
-// 					if (err) return console.error(err); 
-// 					res.render('Vit', {Vit : Vit}); 
-// 				}) 
-// 			}) 
-// 		} 
-// 	});
-// }
-app.listen(PORT, () => {
+  app.post("/vvv",urlencodedParser, function (request, response) {
+	  let name= request.body.name;
+  	Vit.find({name : name}, function (err, vit) { 
+  		if (err) return console.error(err); 
+ 		if (vit.length) { 
+ 			response.render('errUser', {add : true, name : name});             
+				} else { 
+ 			newVit = new Vit({name : name}); 
+  			newVit.save(function (err){ 
+ 	 			if (err) return console.error(err); 
+ 	 			Vit.find(function (err, Vit) { 
+ 	 				if (err) return console.error(err); 
+ 					response.render('Vit', {Vit : Vit}); 
+ 				}) 
+ 	 		}) 
+ 	 	} 
+	  });
+	});
+app.listen(PORT,()=>{
 	console.log(`app started on port ${PORT}`);
 });
+
